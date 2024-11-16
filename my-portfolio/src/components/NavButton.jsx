3fc751409f70302aa4db3b443db8a3ba7a8035af
stanleyavril123 <stanley.avril@outlook.com>
2024-11-16 09:48:16 -0500

    Added navigation Buttons in header

--- my-portfolio/src/components/NavButton.jsx
+++ my-portfolio/src/components/NavButton.jsx
@@ -5,6 +5,17 @@ import React from "react";
 const navButton = ["ABOUT", "PROJECTS", "SKILLS", "CONTACT ME"]
 
 export default function NavButton() {
+    return (
+        <div>
+            <Stack direction="column" spacing={2}>
+            {navButton.map((lable, index) => (
+                <a key={index}>
+                    <Typography>{lable}</Typography>
+                </a>
+            ))} 
+            </Stack>
+        </div>
+    )
 
 
 
